2fac240a6ad2254f6e131a90022ebe52e7e2be00
Daniel <danielotero@users.noreply.github.com>
2019-08-19 20:14:11 +0200

    Preserve given name in curlget

--- dotfiles/.config/zsh/aliases.zsh
+++ dotfiles/.config/zsh/aliases.zsh
@@ -1,3 +1,3 @@
 alias ls="ls --color=auto"
 alias fuck='sudo $(fc -ln -1)'
-alias curlget="curl -O --retry 999 --retry-max-time 0 -L -C -"
+alias curlget="curl -J -O --retry 999 --retry-max-time 0 -L -C -"
