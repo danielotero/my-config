a7a364fb030d106489a8fdd0bfbd31b4c815b732
Daniel <danielotero@users.noreply.github.com>
2020-01-31 11:17:19 +0100

    Discard `infocmp` output

--- dotfiles/.config/zsh/tmux-on-ssh.zsh
+++ dotfiles/.config/zsh/tmux-on-ssh.zsh
@@ -1,6 +1,6 @@
 if [[ -z "$TMUX" && -n "$SSH_CONNECTION" ]] && type tmux >& /dev/null; then
   # Override the TERM variable if there is no terminfo available
-  if ! infocmp $TERM;
+  if ! infocmp $TERM >& /dev/null;
   then
     TERM=xterm-256color
   fi
