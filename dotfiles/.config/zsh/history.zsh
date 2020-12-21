62a503046f0a7a46b7e8e7875f0817361ac1a27c
Daniel Otero <danielotero@users.noreply.github.com>
2020-12-21 11:05:17 +0100

    Update ZSH history management (defaulting to shared)

--- dotfiles/.config/zsh/history.zsh
+++ dotfiles/.config/zsh/history.zsh
@@ -1,6 +1,12 @@
 HISTFILE=~/.cache/zsh_history
 HISTSIZE=10000
 SAVEHIST=10000
+# Prevents the current line from being saved in the history if it is the same as the previous one
 setopt histignoredups
-setopt appendhistory
+# Appends history list to the history file, rather than replacing it ("inc" for inmediate append instead at exit)
+#setopt appendhistory
+setopt incappendhistory
+# Prevents the current line from being saved if it begins with a space
 setopt histignorespace
+# Reloads the history whenever you use it
+setopt sharehistory
