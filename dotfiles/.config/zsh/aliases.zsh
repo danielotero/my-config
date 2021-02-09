d24dd5ed5ab3472f68962ee31766d95437363025
Daniel Otero <danielotero@users.noreply.github.com>
2021-02-09 09:31:47 +0100

    Add an alias to access dmesg through journalctl
    Since we need root permission to directly read the kernel ring buffer.

--- dotfiles/.config/zsh/aliases.zsh
+++ dotfiles/.config/zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias curlget="curl -J -O --retry 999 --retry-max-time 0 -L -C -"
+alias dmesg="journalctl -ko short-monotonic --no-hostname --no-pager"
 alias fuck='sudo $(fc -ln -1)'
 alias ls="ls --color=auto"
 alias vim="nvim"
